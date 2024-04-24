3cc07ddc394678e78e0e852363b79e0e9c4d32da
Andy <68723584+LovelyAndy@users.noreply.github.com>
2024-04-24 12:09:41 +0900

    revert commenting out of v-for

--- src/components/MyTableWithTitle.vue
+++ src/components/MyTableWithTitle.vue
@@ -11,17 +11,9 @@ const props = defineProps<{
 <template>
     <h1 style="font-size: 2rem">{{ title }}</h1>
     <MyTable :data="data" :col="col">
-        <template #editbutton="slotProps">
-            <slot name="editbutton" v-bind="slotProps" />
-        </template>
-        <template #alldata="slotProps">
-            <slot name="alldata" v-bind="slotProps" />
-        </template>
-    </MyTable>
-    <!-- <MyTable :data="data" :col="col">
         <template v-for="slot in Object.keys($slots)" #[slot]="slotProps">
             <slot :name="slot" v-bind="slotProps" />
         </template>
-    </MyTable> -->
+    </MyTable>
     <!-- how to set up the slots so they still work -->
 </template>
